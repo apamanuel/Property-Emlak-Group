79d8debd718d059b43e06f124941349371190fcc
ManuelE <apamanuel@gmail.com>
2023-12-10 06:43:32 -0400

    Layout modified without style

--- client/src/components/shared/Layout.jsx
+++ client/src/components/shared/Layout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-import style from "../shared/Layout.module.css";
 
 const Layout = ({ children })=>{
     return( 
