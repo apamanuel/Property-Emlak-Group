5618c4d9c576af10f56adcd48218a9b703035308
ManuelE <apamanuel@gmail.com>
2023-12-20 01:55:16 -0400

    component FeaturesCard

---
+++ client/src/components/FeaturesCard/FeaturesCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import home from "../../assets/home.png";
+import bed from "../../assets/bed.png";
+import space from "../../assets/optimization.png";
+import style from "../FeaturesCard/FeaturesCard.module.css";
+
+const FeaturesCard = (props)=>{
+    return(
+        <div>
+            Features
+        </div>
+    );
+};
+
+export default FeaturesCard;
