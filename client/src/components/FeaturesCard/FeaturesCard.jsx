de0c2495c6f21baead189daddc2f1929df48372e
ManuelE <apamanuel@gmail.com>
2023-12-23 02:10:46 -0400

    FeaturesCard done

--- client/src/components/FeaturesCard/FeaturesCard.jsx
+++ client/src/components/FeaturesCard/FeaturesCard.jsx
@@ -1,36 +1,31 @@
 import React from "react";
 import bed from "../../assets/bed.png";
 import space from "../../assets/optimization.png";
-import location from "../../assets/location.png";
+import locationIcon from "../../assets/location.png";
 import bathroom from "../../assets/bathroom.png";
 import style from "../FeaturesCard/FeaturesCard.module.css";
 
 
-const FeaturesCard = ()=>{
-    const features = {
-        propertyType: 'apartmen',
-        bedrooms: 3,
-        bathrooms: 2,
-        livingSpace: '200m2',
-        location: 'Istanbul',
-    };
+const FeaturesCard = (props)=>{
+    const { bedrooms, bathrooms, livingSpace, location} = props.features;
+
     return(
         <div className={style.container}>
             <div className={style.iconContainer}>
-                <img src={location} alt='location' className={style.icon} />
-                {features.location}                
+                <img src={locationIcon} alt='location' className={style.icon} />
+                {location}                
             </div>
             <div className={style.iconContainer}>
                 <img src={bed} alt='bed' className={style.icon} />
-                {features.bedrooms}
+                {bedrooms}
             </div>
             <div className={style.iconContainer}>
                 <img src={bathroom} alt='bathroom' className={style.icon} />
-                {features.bathrooms}
+                {bathrooms}
             </div> 
             <div className={style.iconContainer}>
                 <img src={space} alt='space' className={style.icon} />
-                {features.livingSpace}
+                {livingSpace}
             </div>                  
         </div>
     );
