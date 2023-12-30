b2dacdade65431530e1663565567dc8baf64d7e8
ManuelE <apamanuel@gmail.com>
2023-12-30 04:16:31 -0400

    add new property

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -65,7 +65,28 @@ const initialState = {
             price: 2141200,
             ref: 'PTF6338',
             image: "https://i.ibb.co/r3gnzjr/51b22d74d4c8403badeb37294784d37d.jpg",
-        }
+        },
+        {
+            id: 4,
+            title: "Panoramic sea and yalikavak bay view private villas",
+            description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit saepe cum quod quam quia alias voluptatibus veritatis praesentium error fugit nemo quo explicabo porro soluta ut ratione, consequatur harum! Libero.",
+            overview: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+            reasons:{
+                r1:'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                r2:'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                r3: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            features:{
+                propertyType: 'Villa',
+                bedrooms: 3,
+                bathrooms: 2,
+                livingSpace: '150m2',
+                location: 'Istanbul',
+            },
+            price: 856458,
+            ref: 'PTF6387',
+            image: "https://i.ibb.co/Kb7mb5m/ef3c8d22ddce09d239766c637e913dd2-890x440.jpg",
+        },
     ]
 };
 
