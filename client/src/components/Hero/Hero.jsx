06f6aeb1770e5551fbcc78601b396a8b57b318e2
ManuelE <apamanuel@gmail.com>
2024-01-01 08:27:23 -0400

    Button in section hero done

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import style from "../Hero/Hero.module.css";
 
 const Hero = ()=>{
+    const navigate = useNavigate();
+    const handlerBtn = ()=>{
+        navigate('/forSale');
+    };
     return (
         <section className={style.container}>
             <div className={style.heroBody}>
                 <h1 className={style.title}>
                     Investment Potential in Turkey
                 </h1>
-                <button className={style.btn}>
+                <button className={style.btn} onClick={handlerBtn}>
                     ENTER
                 </button>
             </div>
